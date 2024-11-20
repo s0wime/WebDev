894899de23b937d1d3adaca195152e3f9dbd3bb8
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-20 13:01:36 +0200

    add calculating min number in homework 5

--- homework 5/script.js
+++ homework 5/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const btnCalculateMax = document.querySelector(".btn--calculate-max");
+const btnCalculateMinMax = document.querySelector(".btn--calculate-min-max");
 const calculateTextEl = document.querySelector(".calculate-text");
 const maxResultEl = document.querySelector(".calculate-max-result");
+const minResultEl = document.querySelector(".calculate-min-result");
 
 const headingHeaderEl = document.querySelector(".heading-header");
 const headingFooterEl = document.querySelector(".heading-footer");
@@ -19,18 +20,24 @@ function calcRectArea(side1, side2) {
   return `${side1} * ${side2} = ${side1 * side2}`;
 }
 
-btnCalculateMax.addEventListener("click", function () {
-  let maxNumber = 0;
+btnCalculateMinMax.addEventListener("click", function () {
+  const currentNumber = Number(
+    document.querySelector(`.input-number--${0}`).value
+  );
+  let maxNumber = currentNumber;
+  let minNumber = currentNumber;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 1; i < 10; i++) {
     const currentNumber = Number(
       document.querySelector(`.input-number--${i}`).value
     );
     console.log(currentNumber);
     maxNumber = currentNumber > maxNumber ? currentNumber : maxNumber;
+    minNumber = currentNumber < minNumber ? currentNumber : minNumber;
   }
 
   maxResultEl.textContent = maxNumber;
+  minResultEl.textContent = minNumber;
   calculateTextEl.classList.remove("hidden");
 });
 
